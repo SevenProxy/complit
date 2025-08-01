e90b6bae3d0d36e3a20c5c8a694173fa384ec499
SevenProxy <flapbraga@gmail.com>
2025-07-31 21:25:43 -0300

    [git] file README for project.

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,63 +16,63 @@ use std::usize;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
 pub enum Token {
-    #[regex(r"[\t\f]+", logos::skip)]
-    Ignored,
+  #[regex(r"[\t\f]+", logos::skip)]
+  Ignored,
 
-    #[regex(r"\n")]
-    Line,
+  #[regex(r"\n")]
+  Line,
 
-    #[regex(r"//[^\n]*", logos::skip)]
-    Comment,
+  #[regex(r"//[^\n]*", logos::skip)]
+  Comment,
 
-    #[token("armazena_robozinho")]
-    Let,
+  #[token("armazena_robozinho")]
+  Let,
 
-    #[regex("[a-zA-Z_][a-zA-Z0-9_]*", |lex| Some(lex.slice().to_string()))]
-    Identifier(String),
+  #[regex("[a-zA-Z_][a-zA-Z0-9_]*", |lex| Some(lex.slice().to_string()))]
+  Identifier(String),
     
-    /*
-    *  FUNCTIONs
-    */
-    #[token("mostra_na_tela_robozinho")]
-    Print,
+  /*
+   *  FUNCTIONs
+   */
+  #[token("mostra_na_tela_robozinho")]
+  Print,
 
-    /*
-     *  TYPES
-     */
-    #[regex(r#""([^"\\]|\\.)*""#, |lex| {
-        let slice: &'s str = lex.slice();
-        Some(slice[1..slice.len() - 1].to_string())
-    })]
-    Str(String),
+  /*
+   *  TYPES
+   */
+  #[regex(r#""([^"\\]|\\.)*""#, |lex| {
+    let slice: &'s str = lex.slice();
+    Some(slice[1..slice.len() - 1].to_string())
+  })]
+  Str(String),
 
-    #[regex(r"-?[0-9]+", |lex| lex.slice().parse().ok())]
-    Number(i64),
+  #[regex(r"-?[0-9]+", |lex| lex.slice().parse().ok())]
+  Number(i64),
 
-    #[regex("-?[0-9]+\\.[0-9]+", |lex| lex.slice().parse().ok())]
-    Float(f64),
+  #[regex("-?[0-9]+\\.[0-9]+", |lex| lex.slice().parse().ok())]
+  Float(f64),
 
-    #[regex("(true|false)", |lex| lex.slice().parse().ok())]
-    Boolean(bool),
+  #[regex("(true|false)", |lex| lex.slice().parse().ok())]
+  Boolean(bool),
 
-    /*
-     *  SYMBOLS
-     */
-    #[token("=")]
-    Equal,
-    #[token("+")]
-    Plus,
-    #[token("-")]
-    Minus,
-    #[token("*")]
-    Star,
-    #[token("/")]
-    Slash,
-    #[token("(")]
-    LParen,
-    #[token(")")]
-    RParen,
-    #[token(";")]
-    Semicolon,
+  /*
+   *  SYMBOLS
+   */
+  #[token("=")]
+  Equal,
+  #[token("+")]
+  Plus,
+  #[token("-")]
+  Minus,
+  #[token("*")]
+  Star,
+  #[token("/")]
+  Slash,
+  #[token("(")]
+  LParen,
+  #[token(")")]
+  RParen,
+  #[token(";")]
+  Semicolon,
 }
 
